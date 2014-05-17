8352f5c46d8307448a374e036de67c7e7ec6df09
Nathan Muir <ndmuir@gmail.com>
2014-05-17 16:11:40 +1000

    Updated to `celery-shoot` v2.0.1

--- package.js
+++ package.js
@@ -6,6 +6,6 @@ Package.on_use(function (api) {
   api.use('underscore', 'server');
   api.export('CeleryClient', 'server');
   api.export('CeleryClients', 'server');
-  Npm.depends({"node-celery": 'https://github.com/nathan-muir/node-celery/tarball/b7309ae1b0b556312b28d8e769ed04cd37a768e7'});
+  Npm.depends({"celery-shoot": '2.0.1'});
   api.add_files(['server.js'],'server');
 });
